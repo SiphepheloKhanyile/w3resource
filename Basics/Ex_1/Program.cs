3fba5fb3049e9ea9a4b821a54bd199f83ad5be87
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-06-07 04:29:10 +0200

    add: swap numbers

---
+++ Basics/Ex_1/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+program to swap two integer numbers.
+Test Data:
+Input the First Number : 5
+Input the Second Number : 6
+Expected Output:
+After Swapping :
+First Number : 6
+Second Number : 5
+*/
+using System;
+namespace Ex_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Input the First Number : ");
+        int firstNumber =  Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Input the second Number : ");
+        int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+        // int temp = firstNumber;
+        // firstNumber = secondNumber;
+        // secondNumber = temp;
+
+        (secondNumber, firstNumber) = (firstNumber, secondNumber);
+
+        Console.WriteLine("After Swapping :");
+        Console.WriteLine($"First Number: {firstNumber}");
+        Console.WriteLine($"Second Number: {secondNumber}");
+    }
+}
